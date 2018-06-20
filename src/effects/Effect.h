759bb1b550fa7596afbd51806a02d8ca290f376f
Jack Christiansen <christj6@tcnj.edu>
2018-06-19 20:27:38 -0400

    removed Effect.cpp file, and some unused helper macros/constants

--- src/effects/Effect.h
+++ src/effects/Effect.h
@@ -43,8 +43,6 @@ class wxWindow;
 class ShuttleGui;
 class AudacityCommand;
 
-#define BUILTIN_EFFECT_PREFIX wxT("Built-in Effect: ")
-
 class AudacityProject;
 class SelectedRegion;
 class Track;
@@ -57,51 +55,5 @@ class WaveTrack;
 // FIXME:
 
 #define ID_EFFECT_PREVIEW ePreviewID
-
-// Helper macros for defining, reading and verifying effect parameters
-
-#define Param(name, type, key, def, min, max, scale) \
-   static const wxChar * KEY_ ## name = (key); \
-   static const type DEF_ ## name = (def); \
-   static const type MIN_ ## name = (min); \
-   static const type MAX_ ## name = (max); \
-   static const type SCL_ ## name = (scale);
-
-#define PBasic(name, type, key, def) \
-   static const wxChar * KEY_ ## name = (key); \
-   static const type DEF_ ## name = (def);
-
-#define PRange(name, type, key, def, min, max) \
-   PBasic(name, type, key, def); \
-   static const type MIN_ ## name = (min); \
-   static const type MAX_ ## name = (max);
-
-#define PScale(name, type, key, def, min, max, scale) \
-   PRange(name, type, key, def, min, max); \
-   static const type SCL_ ## name = (scale);
-
-#define ReadParam(type, name) \
-   type name = DEF_ ## name; \
-   if (!parms.ReadAndVerify(KEY_ ## name, &name, DEF_ ## name, MIN_ ## name, MAX_ ## name)) \
-      return false;
-
-#define ReadBasic(type, name) \
-   type name; \
-   wxUnusedVar(MIN_ ##name); \
-   wxUnusedVar(MAX_ ##name); \
-   wxUnusedVar(SCL_ ##name); \
-   if (!parms.ReadAndVerify(KEY_ ## name, &name, DEF_ ## name)) \
-      return false;
-
-#define ReadAndVerifyEnum(name, list) \
-   int name; \
-   if (!parms.ReadAndVerify(KEY_ ## name, &name, DEF_ ## name, list)) \
-      return false;
-
-#define ReadAndVerifyInt(name) ReadParam(int, name)
-#define ReadAndVerifyDouble(name) ReadParam(double, name)
-#define ReadAndVerifyFloat(name) ReadParam(float, name)
-#define ReadAndVerifyBool(name) ReadBasic(bool, name)
-#define ReadAndVerifyString(name) ReadBasic(wxString, name)
 
 #endif
